dc05f53684ed4892f1ca9d0905974b977a8d30e7
monsieurwai <monsieurwai@gmail.com>
2020-07-07 15:43:20 +0200

    checkbox debut

---
+++ components/AppCheckbox.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <div class="relative">
+      <input
+        type="checkbox"
+        :name="name"
+      />
+      <div
+        class="checkbox-custom block h-5 w-5 bg-lightWhite border-solid border cursor-pointer rounded border-lightGrey"
+      ></div>
+    </div>
+    <label :for="name">
+      <slot>{{label}}</slot>
+    </label>
+  </div>
+</template>
+
+<script>
+export default {
+  data: {
+    isChecked: false
+  },
+  props: {
+    name: {
+      type: String,
+      default: "name"
+    },
+    label: {
+      type: String,
+      default: ""
+    }
+  }
+};
+</script>
